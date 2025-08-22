161203cc0fa6b0c32f4f149ec0c816ea9f0258be
yeaminsarder <yeaminsardersr@gmail.com>
2025-08-22 20:19:11 +0600

    cubesdf

--- sdf.py
+++ sdf.py
@@ -13,3 +13,11 @@ class CircleSdf(Sdf):
         point = vec3_add(point, self.position)
         return length(*point) - self.radius
 
+class CubeSdf(Sdf):
+    def __init__(self,corner, position = (0,0,0)):
+        self.corner = corner
+        self.position = position
+    def sdf(self, point):
+        point = vec3_add(point, self.position)
+        q = vec3_sub(vec3_op1(abs,point), self.corner)
+        return length(*vec3_op2(max,q,(0,0,0))) + min(max(*q),0)
