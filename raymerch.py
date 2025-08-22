2333d25a7131c1a323c44f62516eb0fad537187c
yeaminsarder <yeaminsardersr@gmail.com>
2025-08-22 21:09:38 +0600

    raygen function

--- raymerch.py
+++ raymerch.py
@@ -22,3 +22,20 @@ def raymerch(point, direction, minsdf, max_iteration = 100, threshold = 0.00001)
         p = vec3_add(point, vec3_scaler_mul(d, di)) # point + d*di
         i += 1
     return vec3_add(point, vec3_scaler_mul(d, di)) # point + d*di
+
+def raygen(x,y,z,a,b,c,p,q,r,resx=50,resy=50):
+    """
+    generate rays. yields raydirections based on
+    camera position: (x,y,z)
+    look at target: (a,b,c) and
+    up vector: (p,q,r)
+    """
+    mid = normalize(*vec3_sub((a,b,c), (x,y,z)))
+    v = (p,q,r)
+    dy = normalize(*vec3_sub(v,vec3_scaler_mul(vec3_dot(mid, (p,q,r)),mid)))
+    dx = vec3_cross(mid,dy)
+    for i in range(resy):
+        for j in range(resx):
+            fy = -0.5 + i/resy
+            fx = -0.5 + j/resx
+            yield normalize(*vec3_add(vec3_add(mid, vec3_scaler_mul(fx,dx)),vec3_scaler_mul(fy,dy)))
